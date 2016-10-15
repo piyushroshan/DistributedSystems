5bd2eaeedb7e78055aa41e6e57d5380ecedc0054
piyushroshan <piyush.roshan@gmail.com>
2016-10-15 21:51:28 +0530

    Auto fetch hostname

--- spawn_client.sh
+++ spawn_client.sh
@@ -2,6 +2,6 @@
 for count in `seq 1 100`
 do
         echo " Spawning client $count"
-        ./client/client "localhost" "$count" "$count"  &>/dev/null &
+        ./client/client "$HOSTNAME" "$count" "$count"  &>/dev/null &
         echo $!
 done
